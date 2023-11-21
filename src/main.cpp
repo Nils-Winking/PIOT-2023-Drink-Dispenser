3f39703fb4ae9f7d657f61a975adec02435d5cde
Nils Winking <nils@winking-online.de>
2023-11-21 22:23:40 +0100

    Add sample code for fetching data from the web

--- src/main.cpp
+++ src/main.cpp
@@ -1,157 +1,145 @@
-/**************************************************************************/
-/*!
-    @file     readntag203.pde
-    @author   KTOWN (Adafruit Industries)
-    @license  BSD (see license.txt)
+/*
+  Web client
 
-    This example will wait for any NTAG203 or NTAG213 card or tag,
-    and will attempt to read from it.
+  This sketch connects to a website (http://www.google.com)
+  using the WiFi module.
 
-    This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-    This library works with the Adafruit NFC breakout
-      ----> https://www.adafruit.com/products/364
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the WiFi.begin() call accordingly.
 
-    Check out the links above for our tutorials and wiring diagrams
-    These chips use SPI or I2C to communicate.
-
-    Adafruit invests time and resources providing this open source code,
-    please support Adafruit and open-source hardware by purchasing
-    products from Adafruit!
-*/
-/**************************************************************************/
-#define MIFAREDEBUG
-#define PN532DEBUG
-
-#include <Wire.h>
-#include <SPI.h>
-#include "Adafruit_PN532.h"
-
-// If using the breakout with SPI, define the pins for SPI communication.
-#define PN532_SCK (2)
-#define PN532_MOSI (3)
-#define PN532_SS (10)
-#define PN532_MISO (5)
-
-// If using the breakout or shield with I2C, define just the pins connected
-// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
-#define PN532_IRQ (2)
-#define PN532_RESET (3)  // Not connected by default on the NFC Shield
-
-// Uncomment just _one_ line below depending on how your breakout or shield
-// is connected to the Arduino:
-
-// Use this line for a breakout with a software SPI connection (recommended):
-// Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-
-// Use this line for a breakout with a hardware SPI connection.  Note that
-// the PN532 SCK, MOSI, and MISO pins need to be connected to the Arduino's
-// hardware SPI SCK, MOSI, and MISO pins.  On an Arduino Uno these are
-// SCK = 13, MOSI = 11, MISO = 12.  The SS line can be any digital IO pin.
-Adafruit_PN532 nfc(PN532_SS);
-
-// Or use this line for a breakout or shield with an I2C connection:
-//Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+  This example is written for a network using WPA encryption. For
+  WEP or WPA, change the WiFi.begin() call accordingly.
 
 
-void setup(void) {
-  Serial.begin(115200);
-  while (!Serial) delay(10);  // for Leonardo/Micro/Zero
+  created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
 
-  Serial.println("Hello!");
+  Find the full UNO R4 WiFi Network documentation here:
+  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#wi-fi-web-client
+ */
 
-  nfc.begin();
 
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (!versiondata) {
-    Serial.print("Didn't find PN53x board");
-    while (1)
-      ;  // halt
-  }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5");
-  Serial.println((versiondata >> 24) & 0xFF, HEX);
-  Serial.print("Firmware ver. ");
-  Serial.print((versiondata >> 16) & 0xFF, DEC);
-  Serial.print('.');
-  Serial.println((versiondata >> 8) & 0xFF, DEC);
+#include "WiFiS3.h"
 
-  Serial.println("Waiting for an ISO14443A Card ...");
-  nfc.SAMConfig();
+
+#include "arduino_secrets.h"
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "jsonplaceholder.typicode.com";    // name address for Google (using DNS)
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+
+/* -------------------------------------------------------------------------- */
+void printWifiStatus() {
+  /* -------------------------------------------------------------------------- */
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
 
-void loop(void) {
-  uint8_t success = 1;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength = 7;                    // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
-  // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
-  // the UID, and uidLength will indicate the size of the UUID (normally 7)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  delay(1000);
+/* -------------------------------------------------------------------------- */
+void setup() {
+/* -------------------------------------------------------------------------- */
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
-  if (success) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");
-    Serial.print(uidLength, DEC);
-    Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
-    Serial.println("");
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
 
-    if (uidLength == 7) {
-      uint8_t data[32];
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
 
-      // We probably have an NTAG2xx card (though it could be Ultralight as well)
-      Serial.println("Seems to be an NTAG2xx tag (7 byte UID)");
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
 
-      // NTAG2x3 cards have 39*4 bytes of user pages (156 user bytes),
-      // starting at page 4 ... larger cards just add pages to the end of
-      // this range:
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
 
-      // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
+  printWifiStatus();
 
-      // TAG Type       PAGES   USER START    USER STOP
-      // --------       -----   ----------    ---------
-      // NTAG 203       42      4             39
-      // NTAG 213       45      4             39
-      // NTAG 215       135     4             129
-      // NTAG 216       231     4             225
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /todos/1 HTTP/1.1");
+    client.println("Host: jsonplaceholder.typicode.com");
+    // client.println("Content-Type: application/json");
+    client.println("Connection: close");
+    client.println();
+  }
+}
 
-      for (uint8_t i = 4; i < 42; i++) {
-        success = nfc.ntag2xx_ReadPage(i, data);
-
-        // Display the current page number
-        Serial.print("PAGE ");
-        if (i < 10) {
-          Serial.print("0");
-          Serial.print(i);
-        } else {
-          Serial.print(i);
-        }
-        Serial.print(": ");
-
-        // Display the results, depending on 'success'
-        if (success) {
-          // Dump the page data
-          nfc.PrintHexChar(data, 4);
-        } else {
-          Serial.println("Unable to read the requested page!");
-        }
-        delay(200);
-      }
-    } else {
-      Serial.println("This doesn't seem to be an NTAG203 tag (UUID length != 7 bytes)!");
+/* just wrap the received data up to 80 columns in the serial print*/
+/* -------------------------------------------------------------------------- */
+void read_response() {
+/* -------------------------------------------------------------------------- */
+  uint32_t received_data_num = 0;
+  while (client.available()) {
+    /* actual data reception */
+    char c = client.read();
+    /* print data to serial port */
+    Serial.print(c);
+    /* wrap data to 80 columns*/
+    received_data_num++;
+    if(received_data_num % 80 == 0) {
+      Serial.println();
     }
+  }
+}
 
-    // Wait a bit before trying again
-    Serial.println("\n\nSend a character to scan another tag!");
-    Serial.flush();
-    while (!Serial.available())
-      ;
-    while (Serial.available()) {
-      Serial.read();
-    }
-    Serial.flush();
+/* -------------------------------------------------------------------------- */
+void loop() {
+/* -------------------------------------------------------------------------- */
+  read_response();
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
   }
 }
